4ffcbc528d39e9b78dd9c671c88d3cff815c6e8e
Rubens Rabelo <rubensrabelo123@gmail.com>
2026-04-09 12:27:43 -0300

    study: class

---
+++ kotlin/04-class/main.kt
@@ -0,0 +1,55 @@
+interface ProgressPrintable {
+    val progressText: String
+
+    fun printProgressBar()
+}
+
+enum class Difficulty {
+    EASY,
+    MEDIUM,
+    HARD
+}
+
+class Quiz: ProgressPrintable {
+    override val progressText: String
+        get() = "$answered of $total answered"
+
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+
+    companion object StudentProgress {
+        var total: Int = 10
+        var answered: Int = 3
+    }
+
+    override fun printProgressBar() {
+        repeat(Quiz.answered) { print("▓") }
+        repeat(Quiz.total - Quiz.answered) { print("▒") }
+        println()
+        println(progressText)
+    }
+}
+
+data class Question<T>(
+    val questionText: String,
+    val answer: T,
+    var difficulty: Difficulty
+)
+
+/*
+val Quiz.StudentProgress.progressText: String
+    get() = "${answered} of ${total} answered"
+
+fun Quiz.StudentProgress.printProgressBar() {
+    repeat(Quiz.answered) { print("▓") }
+    repeat(Quiz.total - Quiz.answered) { print("▒") }
+    println()
+    println(Quiz.progressText)
+}
+*/
+
+fun main() {
+    Quiz().printProgressBar()
+}
+
